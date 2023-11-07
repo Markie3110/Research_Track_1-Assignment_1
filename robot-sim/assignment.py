a468dca6e089156ecee2e088ea4aab310f7bfe96
Markie3110 <markdsouza3110@gmail.com>
2023-11-07 13:18:00 +0100

    Added functions
    Continued building the code

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -3,17 +3,52 @@ from __future__ import print_function
 import time
 from sr.robot import *
 
-R = Robot()
+R = SimRobot()
 
 def detect_tokens(unplaced_boxes, placed_boxes):
     '''
+    A function designed to update unplaced_boxes for any undetected tokens while the robot is moving.
+
+    Args: 
+    -> unplaced_boxes(int): A list containing the codes of the unplaced boxes
+    -> placed_boxes(int): A list containing the codes of the placed boxes
+
+    Output: None
     '''
     for token in R.see():
         if ((token.info.code not in unplaced_boxes) and (token.info.code not in placed_boxes)):
             unplaced_boxes.append(token.info.code)
     return 0
 
-def drive(speed, time):
+def scan(speed, seconds):
+    '''
+    '''
+    turn(speed, seconds)
+    detect_closest_token()
+    turn(-speed, seconds*2)
+    detect_closest_token
+    turn(speed, seconds)
+    time.sleep(seconds)
+    return 0
+
+def detect_closest_token(min_code, min_dist, min_rot_y):
+    '''
+    '''
+    i = 0
+    for token in R.see():
+        if (i == 0):
+            min_code = token.info.code
+            min_dist = token.dist
+            min_rot_y = token.rot_y
+            i = i + 1
+            continue
+        if (token.dist <= min_dist):
+            min_code = token.index.code
+            min_dist = token.dist
+            min_rot_y = token.rot_y_
+    return 0
+
+def drive(speed, seconds):
     """
     Function for setting a linear velocity
     
@@ -27,7 +62,7 @@ def drive(speed, time):
     R.motors[0].m1.power = 0
     return 0
 
-def turn(speed, time):
+def turn(speed, seconds):
     """
     Function for setting an angular velocity
     
@@ -45,7 +80,7 @@ def turn(speed, time):
 def main():
     unplaced_boxes = [] # Create a list of unplaced token codes
     placed_boxes = [] # Create a list of placed token codes
-
+    min_code, min_dist, min_rot_y = 0, 0, 0 
 # The robot is continously scaning while moving for any new undetected tokens
 
 # Have the robot pan left with a certain speed for a certain number of seconds to detect new boxes and their distances
