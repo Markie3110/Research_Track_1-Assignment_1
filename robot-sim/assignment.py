d7da3357cbc5a18426fa830dacf2a89a7e448ed0
Markie3110 <markdsouza3110@gmail.com>
2023-11-09 03:30:57 +0100

    Completed Code/1.0a
    Code that runs the requirements of the assignment. Scope for further improvement in terms of code optimization and task execution of the robot. Comments and function descriptions are still pending.

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -11,8 +11,8 @@ def detect_boxes(unplaced_boxes, placed_boxes):
     A function designed to update unplaced_boxes for any undetected boxes while the robot is moving.
 
     Args: 
-    -> unplaced_boxes(int): A list containing the codes of the unplaced boxes
-    -> placed_boxes(int): A list containing the codes of the placed boxes
+    -> unplaced_boxes(list[int]): A list containing the codes of the unplaced boxes
+    -> placed_boxes(list[int]): A list containing the codes of the placed boxes
 
     Output: None
     '''
@@ -21,10 +21,24 @@ def detect_boxes(unplaced_boxes, placed_boxes):
             unplaced_boxes.append(box.info.code)
     return 0
 
+
 def scan_for_closest_box(speed, seconds, desired_angular_disp, unplaced_boxes, placed_boxes):
     '''
-    Angular_velocity= ang-disp/time
+    A function that pans the robot left and right by a certain displacement to find the box closest to it.
+
+    Args:
+    -> speed (double): The speed of the wheels
+	-> seconds (double): The time interval
+    -> desired_angular_disp (double): The (approximate) angular displacement by which the robot should rotate about in both directions
+    -> unplaced_boxes(list[int]): A list containing the codes of the unplaced boxes
+    -> placed_boxes(list[int]): A list containing the codes of the placed boxes
+
+    Output:
+    -> min_code(int): The code of the box closest to the robot
+    -> min_dist(double): The distance of the robot to the box closes to it
+    -> min_rot_y(double): The anglular difference between the robot and the box closest to it
     '''
+
     actual_angular_disp = 0
     i, min_code, min_dist, min_rot_y = 0, 0, 0, 0
     while (actual_angular_disp < desired_angular_disp):
@@ -46,8 +60,13 @@ def scan_for_closest_box(speed, seconds, desired_angular_disp, unplaced_boxes, p
         actual_angular_disp = actual_angular_disp + (speed * seconds)
     return min_code, min_dist, min_rot_y
 
+
 def detect_closest_box(i, min_code, min_dist, min_rot_y):
     '''
+    A function that returns the closest box to the robot from the list of boxes the robot can see at that instant.
+
+    Args:
+    -> i(int): A flag 
     '''
     for box in R.see():
         if (i == 0):
@@ -65,19 +84,23 @@ def detect_closest_box(i, min_code, min_dist, min_rot_y):
 def find_box(target_box):
     '''
     '''
-    dist, rot_y = -1, -1
+    dist, rot_y = -1, -1 
     for box in R.see():
+        #print('Current box: ', box.info.code, box.dist)
         if (box.info.code == target_box):
             dist = box.dist
             rot_y = box.rot_y
+            #print(box.info.code, dist, rot_y)
+            break
     return dist, rot_y
 
 def drive(speed, seconds):
     """
     Function for setting a linear velocity
     
-    Args: speed (int): the speed of the wheels
-	  seconds (int): the time interval
+    Args: 
+    -> speed (double): The speed of the wheels
+	-> seconds (double): The time interval
     """
     R.motors[0].m0.power = speed
     R.motors[0].m1.power = speed
@@ -90,8 +113,9 @@ def turn(speed, seconds):
     """
     Function for setting an angular velocity
     
-    Args: speed (int): the speed of the wheels
-	  seconds (int): the time interval
+    Args:
+    -> speed (double): The speed of the wheels
+	-> seconds (double): The time interval
     """
     R.motors[0].m0.power = speed
     R.motors[0].m1.power = -speed
@@ -103,64 +127,87 @@ def turn(speed, seconds):
 def move_to_target(target_box, angle_threshold, distance_threshold):
     '''
     '''
-    dist, rot_y = find_box(target_box)
+    actual_ang_disp, dist, rot_y = 0, -1, -1
+    while (actual_ang_disp < 120.0):
+        dist, rot_y = find_box(target_box)
+        if (dist != -1 and rot_y != -1):
+            #print('Found box:', target_box)
+            break
+        turn(15, 0.05)
+        #print('Not found')
+        actual_ang_disp= actual_ang_disp + (15 * 0.05)
     if (dist == -1 and rot_y == -1):
-        exit(-1)
+        #print('Not found')
+        return -1
     while (dist >= distance_threshold):
         if (rot_y < (-angle_threshold)):
-            turn(-15,0.05)
+            turn(-15,0.01)
+            #print('Turning')
         elif (rot_y > angle_threshold):
-            turn(15,0.05)
+            turn(15,0.01)
+            #print('Turning')
         else:
             drive(60,0.05)
-    dist, rot_y = find_box()
+        dist, rot_y = find_box(target_box)
+        if (dist == -1 and rot_y == -1):
+            desired_angular_disp = 30
+            actual_angular_disp = 0
+            while (actual_angular_disp < desired_angular_disp):
+                turn(15, 0.02)
+                dist, rot_y = find_box(target_box)
+                if (dist != -1 and rot_y != -1):
+                    break
+                actual_angular_disp = actual_angular_disp + (15 * 0.05)
+            actual_angular_disp = 0
+            while (actual_angular_disp < desired_angular_disp*2):
+                turn(-15, 0.02)
+                dist, rot_y = find_box(target_box)
+                if (dist != -1 and rot_y != -1):
+                    break
+                actual_angular_disp = actual_angular_disp + (15 * 0.05)
+            actual_angular_disp = 0
+            while (actual_angular_disp < desired_angular_disp):
+                turn(15, 0.02)
+                dist, rot_y = find_box(target_box)
+                if (dist != -1 and rot_y != -1):
+                    break
+                actual_angular_disp = actual_angular_disp + (15 * 0.05)
+        print(dist, rot_y)
+    print('Reached!')
+    return 1
 
 def main():
     unplaced_boxes = [] # Create a list codes of the unplaced box 
     placed_boxes = [] # Create a list of codes of the placed box 
     a_th = 2.0 # Threshold for the control of the orientation
     d_th = 0.4 # Threshold for the control of the linear distance
+    prime_d_th = 0.53
+    grabbed_box = 0 # Variable to depict the code of the box currently held by the robot
 
     # Scan for the closest box and mark it as the prime box to which we bring the other boxes
     min_code, min_dist, min_rot_y = scan_for_closest_box(15, 0.05, 12.0, unplaced_boxes, placed_boxes)
     unplaced_boxes.remove(min_code)
     placed_boxes.append(min_code)
 
-    # Take the first box in the undetected boxes list and move towards it
-    # When the robot is near the box grab it
-    # Find the prime box and move towards it
-    # When the robot is near the prime box release the target and back the robot up a bit
-
-# The robot is continously scaning while moving for any new undetected tokens
-
-# Have the robot pan left with a certain speed for a certain number of seconds to detect new boxes and their distances
-# Have the robot pan right as before to detect new tokens and distances
-
-# Find the token with the closest position and mark it as the prime token
-# Place the prime token code in the placed token list
-
-# Extract the tokens the robot can see
-# Take the first token 
-# -> If the length of the list is not 0 and the token is not in placed token
-# -> Get its distance and angle
-
-# Move the robot to the target tokens position
-# Grab the token
-# Rotate the robot counterclockwise until it finds the prime token
-# Get the prime tokens position and angle
-# Move the robot to the prime tokens position within a threshold
-
-# Release the token and have the robot back up by a certain distance
-# Move the token code to the placed token list
-# Check if unplaced token list is empty
-# -> Yes then exit the program and print an exit message
-# -> No, repeat the program
-
-# Exceptions: The robot does not detect any boxes
+    while (len(unplaced_boxes) != 0):
+        # Take the first box in the undetected boxes list and move towards it
+        for box in unplaced_boxes:
+            #print(box)
+            status = move_to_target(box, a_th, d_th)
+            if status == 1:
+                # When the robot is near the box grab it
+                R.grab()
+                grabbed_box = box
+                break
+        # Find the prime box and move towards it
+        move_to_target(min_code, a_th, prime_d_th)
+        # When the robot is near the prime box release the target and back the robot up a bit
+        R.release()
+        prime_d_th = prime_d_th + 0.05
+        placed_boxes.append(grabbed_box)
+        unplaced_boxes.remove(grabbed_box)
+        drive(-60, 0.5)
+    else:
+        print('Done!')
 
 main()
-
-'''
-while 1:
-    pass
-'''
